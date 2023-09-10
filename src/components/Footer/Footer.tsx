fb5abdd25b72ddb8fe63c97c9077f601fa85c410
DvlprMrn <rplc.999@outlook.com>
2023-09-10 13:05:56 -0400

    final fixes /footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {useLocation} from 'react-router-dom';
 import "./Footer.css";
 import Drops from "../Drops/Drops";
+import nasa from "../../assets/icons/nasa.webp";
+import who from "../../assets/icons/who.webp";
+import linkedin from "../../assets/icons/linkedin.webp";
 
 const Footer: React.FC = () => {
     const location = useLocation();
@@ -17,8 +20,28 @@ const Footer: React.FC = () => {
 
     return (
         <footer className="footer">
-            <div className={footerClass}>
+            <div className={`top-footer ${footerClass}`}>
                 <div className={`animation ${animationClass}`}><Drops/></div>
+                <h3>Learn more about climate change here:</h3>
+                <div className="footer-logo">
+                    <div className="footer-image">
+                        <a href="https://climate.nasa.gov/" className="footer-link"target="_blank" rel="noopener noreferrer">
+                            <img src={nasa} alt="NASA logo"/>
+                        </a>
+                        <a href="https://www.who.int/health-topics/climate-change#tab=tab_1" className="footer-link" target="_blank" rel="noopener noreferrer">
+                            <img src={who} alt="WHO logo"/>
+                        </a>
+                    </div>
+                </div>
+                <div className="footer-gratitude">
+                    <p>
+                        <a  href="https://www.linkedin.com/in/mari-dvlpr/"className="footer-link" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="LinkedIn logo"/>
+                        </a>
+                        Hey there! If you spot something that doesn't look right, or if you have any questions, I'd love to hear from you. Feel free to contact me!
+                    </p>
+                    <p>Special thanks to Annie Spratt, a UK-based photographer, for generously providing this stunning iceberg photograph for free use.</p>
+                </div>
             </div>
             <div className="lower-footer">
                 <p>2023</p>
